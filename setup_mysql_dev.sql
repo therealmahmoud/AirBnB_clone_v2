3c83777db44548b2a252f6bfbcf97b85c8e0ef59
therealmahmoud~ <moha91875460@gmail.com>
2024-03-19 00:36:35 +0200

    updated

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+SET GLOBAL validate_password.length = 13;
+SET GLOBAL validate_password.policy = 0;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
+USE hbnb_test_db;
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db` TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
