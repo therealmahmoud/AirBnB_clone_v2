4c4043d2419fcb77ba63e17686912e1f7863ba91
therealmahmoud~ <moha91875460@gmail.com>
2024-04-04 21:32:38 +0200

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -48,6 +48,17 @@ server {
         alias /data/web_static/current;
 	index index.html index.htm;
         }
+
+	location /redirect_me {
+        return 301 http://youtube.com/;
+    	}
+
+	 error_page 404 /404.html;
+    	location /404 {
+     	 root /var/www/html;
+     	 internal;
+    	}
+
 }
 " >> /etc/nginx/sites-available/default
 
