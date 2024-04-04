3c5d4be0eb19782fc38fd4f75eb82625e828ddb3
therealmahmoud~ <moha91875460@gmail.com>
2024-04-04 21:34:34 +0200

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -37,30 +37,27 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-echo "
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-	root   /var/www/html;
-	index  index.html index.htm;
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-        location /hbnb_static {
+    location /hbnb_static {
         alias /data/web_static/current;
-	index index.html index.htm;
-        }
+        index index.html index.htm;
+    }
 
-	location /redirect_me {
+    location /redirect_me {
         return 301 http://youtube.com/;
-    	}
+    }
 
-	 error_page 404 /404.html;
-    	location /404 {
-     	 root /var/www/html;
-     	 internal;
-    	}
-
-}
-" >> /etc/nginx/sites-available/default
-
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
